625fdc5e88ba6c5f3ebbe784ff932664bac79749
srujana0101@gmail.com <srujana0101@gmail.com@68febd8b-95e1-6aca-4479-cf943fe5d08d>
2014-08-13 02:09:11 +0000

    control structures example

---
+++ trunk/CoreJavaBatch2Training/src/com/basic/controlstructures/ControlStructures.java
@@ -0,0 +1,36 @@
+package com.basic.controlstructures;
+
+public class ControlStructures {
+
+	public int sno;
+	public String sname;
+	public String department;
+	public int percentage;
+	
+	
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String deparment) {
+		this.department = deparment;
+	}
+	public int getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
+	
+}
