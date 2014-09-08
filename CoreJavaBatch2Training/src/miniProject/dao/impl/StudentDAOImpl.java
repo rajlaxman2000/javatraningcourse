3cc71cc6b6cebb949a758d7941ce94ed4ce01c70
srujana0101@gmail.com <srujana0101@gmail.com@68febd8b-95e1-6aca-4479-cf943fe5d08d>
2014-09-08 23:39:13 +0000

    project: DB connection interface added

--- CoreJavaBatch2Training/src/miniProject/dao/impl/StudentDAOImpl.java
+++ CoreJavaBatch2Training/src/miniProject/dao/impl/StudentDAOImpl.java
@@ -1,27 +1,114 @@
 package miniProject.dao.impl;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import miniProject.dao.StudentDAO;
+import miniProject.dto.Dbconnection;
 import miniProject.dto.Student;
 
-public class StudentDAOImpl implements StudentDAO {
-
+public class StudentDAOImpl extends Dbconnection implements StudentDAO {
+	
+	PreparedStatement preparedStatement=null;	
+	Statement statement = null;	
+	ResultSet rs		= null;
+	
+	private int id=0;
+	private String studentName=null;
+	private int  branchLoc=0;
+	Student student = new Student();
+		
 	public Student getStudentByName(String name) {
 		
-		return null;
-	}
+		
+		
+		String sql = "SELECT * FROM student where studentName=?";
+		
+		try{
+		
+			preparedStatement =  getConnection().prepareStatement(sql);
+			preparedStatement.setString(1, name);
+			rs = preparedStatement.executeQuery();
+			
+			while (rs.next()) {
+				id = rs.getInt("studentID");
+				studentName = rs.getString("studentName");
+				branchLoc = rs.getInt("branchLocation");
+			}
+			student = new Student(id,studentName,branchLoc);
+			
+			
+		}catch (ClassNotFoundException e) {
+			// TODO: handle exception
+		}catch (SQLException se) {
+			// TODO: handle exception
+		}finally{
+			closeThingsStatement(preparedStatement);
+			closeThingsResultset(rs);
+		}
+		return (Student) student;
+		}
 
 	@Override
-	public Student getStudentById(int id) {
+	public Student getStudentById(int stuid) {
 		
-		return null;
+		String sql = "SELECT * FROM student where studentID=?";
+		
+		try{
+		
+			preparedStatement =  getConnection().prepareStatement(sql);
+			preparedStatement.setInt(1, stuid);
+			rs = preparedStatement.executeQuery();
+			
+			while (rs.next()) {
+				id = rs.getInt("studentID");
+				studentName = rs.getString("studentName");
+				branchLoc = rs.getInt("branchLocation");
+			}
+			student = new Student(id,studentName,branchLoc);
+			
+			
+		}catch (ClassNotFoundException e) {
+			// TODO: handle exception
+		}catch (SQLException se) {
+			// TODO: handle exception
+		}finally{
+			closeThingsStatement(preparedStatement);
+			closeThingsResultset(rs);
+		}
+		return (Student) student;
 	}
 
 	@Override
 	public List<Student> getAllStudents() {
 		
-		return null;
+		String sql ="select * from student";
+		List<Student> students = new ArrayList<Student>();
+		try{
+			
+			preparedStatement =  getConnection().prepareStatement(sql);
+			rs = preparedStatement.executeQuery(sql);
+			while (rs.next()) {
+				id = rs.getInt("studentID");
+				studentName = rs.getString("studentName");
+				branchLoc = rs.getInt("branchLocation");
+				student = new Student(id,studentName,branchLoc);
+				students.add(student);
+			}
+				
+		}catch (ClassNotFoundException e) {
+			// TODO: handle exception
+		}catch (SQLException se) {
+			// TODO: handle exception
+		}finally{
+			closeThingsStatement(preparedStatement);
+			closeThingsResultset(rs);
+		}	
+		return students;
 	}
 
 	@Override
