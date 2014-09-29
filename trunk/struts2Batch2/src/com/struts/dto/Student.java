16e534ab4a5e83a0e797a4f53f7ff2c3ac9b8fe2
rajlaxmaniitg@gmail.com <rajlaxmaniitg@gmail.com@68febd8b-95e1-6aca-4479-cf943fe5d08d>
2014-09-29 16:56:48 +0000

    Initial Struts2 frame work set up

---
+++ trunk/struts2Batch2/src/com/struts/dto/Student.java
@@ -0,0 +1,57 @@
+package com.struts.dto;
+
+public class Student {
+	
+	
+	private String name;
+	
+	private int sno;
+	
+	private int age;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the sno
+	 */
+	public int getSno() {
+		return sno;
+	}
+
+	/**
+	 * @param sno the sno to set
+	 */
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+
+}
